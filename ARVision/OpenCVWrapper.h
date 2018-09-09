639dd2812af433ab5af0253c2c50b17806de63c1
Ian Applebaum <Ian.tyler@electricbluefish.com>
2018-09-08 22:22:48 -0400

    openCV Header files

---
+++ ARVision/OpenCVWrapper.h
@@ -0,0 +1,22 @@
+//
+//  OpenCVWrapper.h
+//  ARVision
+//
+//  Created by Ian Applebaum on 9/8/18.
+//  Copyright © 2018 PennAppsXVIII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVWrapper : NSObject
+//Defifne here interface with openCV
+
+//Function to get OpenCV Version
++(NSString *) openCVVersionString;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
